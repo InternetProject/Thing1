aa24856adc87e42381fa09c9623d6086d2685f26
QQ <QQ@WINDOWS-GHU8OJH>
2016-12-05 02:13:48 -0800

    Home page shell

--- Thing1/Controllers/HomeController.cs
+++ Thing1/Controllers/HomeController.cs
@@ -1,16 +1,29 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
+using Thing1.Models;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace Thing1.Controllers
 {
     public class HomeController : Controller
     {
+        private user_managementEntities1 db = new user_managementEntities1();
+
         public ActionResult Index()
         {
-            return View();
+            /*if(!Request.IsAuthenticated)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.Unauthorized); // should change this later.
+            }
+            */
+            var userid = "0";//User.Identity.GetUserId();
+            var clubMemberships = db.ClubMemberships.Include(c => c.Club);
+            return View(clubMemberships.ToList());//.Where(c => c.UserId == userid).ToList());
         }
     }
 }
---
+++ Thing1/Scripts/Site.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function(){
+
+    $(".expandable-menu dt a").click(function(){
+        $(this).parent().parent().children("dd").toggleClass("hidden");
+        $(this).toggleClass("is-collapsed");
+        if ($(this).hasClass("is-collapsed"))
+            $(this).text("[+]");
+        else
+            $(this).text("[-]");
+        return false;
+    });
+
+    $(".expandable-menu dt a").click();
+});
