ba21e0d54621c34de276df5a9caedc86c6da415f
QQ <QQ@WINDOWS-GHU8OJH>
2016-12-07 13:59:46 -0800

    changed header

--- Thing1/Controllers/HomeController.cs
+++ Thing1/Controllers/HomeController.cs
@@ -21,7 +21,7 @@ namespace Thing1.Controllers
                 return new HttpStatusCodeResult(HttpStatusCode.Unauthorized); // should change this later.
             }
             */
-            var userid = "0";//User.Identity.GetUserId();
+            var userid = User.Identity.GetUserId();
            // var clubMemberships = db.ClubMemberships.Include(c => c.Club);
             return View();// clubMemberships.ToList());//.Where(c => c.UserId == userid).ToList());
         }
