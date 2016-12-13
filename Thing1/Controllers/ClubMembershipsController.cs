12ca97d30bd8ba45ef8ed52cb10fcab7ce0ee05d
Juyong Jin <juyong.jin.2017@anderson.ucla.edu>
2016-12-13 15:03:48 -0800

    Finance team first join page update

--- Thing1/Controllers/ClubMembershipsController.cs
+++ Thing1/Controllers/ClubMembershipsController.cs
@@ -44,6 +44,100 @@ namespace Thing1.Controllers
             return View(clubMembership);
         }
 
+        [Authorize]
+        // GET: ClubMemberships/SelectMembershipOptions/5
+        public ActionResult SelectMembershipOptions(int? clubId)
+        {
+            if (clubId == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Club club = db.Clubs.Find(clubId);
+            if (club == null)
+            {
+                return HttpNotFound();
+            }
+
+            // CHECK LOGIC SHOULD COME HERE
+            // What if the user already has a membership option
+
+            ViewBag.ClubId = club.Id;
+            ViewBag.ClubName = club.name;
+            ViewBag.ClubNickName = club.nickname;
+            ViewBag.MembershipOptions = club.MembershipOptions.ToList();
+
+            //Serialize
+            Session["Club Object"] = club;
+
+            return View();
+        }
+
+        // POST: ClubMemberships/SelectMembershipOptions
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SelectMembershipOptions([Bind(Include = "Id,UserId,ClubId,TermDate,JoinDate,Description,HasAccessToFinance,CanEditClubData,RoleID,MembershipOption,Violation")] ClubMembership clubMembership)
+        {
+
+            if (ModelState.IsValid)
+            {
+                clubMembership.TermDate = DateTime.Now;  // What is the TemrDate supposed to do?
+                clubMembership.JoinDate = DateTime.Now;
+
+                //db.ClubMemberships.Add(clubMembership);
+                //db.SaveChanges();
+
+                // REDUNDANT CHECK LOGIC SHOULD COME HERE
+                // Do we need check if the user chooses the same membership option he or she already has?
+
+
+                // Serialize
+                decimal price = -10000;
+
+                //List<Thing1.Models.MembershipOption> list = db.MembershipOptions.Where(n => n.ClubId == clubMembership.ClubId && n.Option == clubMembership.MembershipOption).ToList();
+                //price = list.First().Price;
+                price = db.MembershipOptions.Where(n => n.ClubId == clubMembership.ClubId && n.Option == clubMembership.MembershipOption).ToList().First().Price;
+
+                if (price == -10000)
+                {
+                    return HttpNotFound();
+                }
+
+                Session["ClubMembership Object"] = clubMembership;
+                Session["Price"] = price;
+
+                return RedirectToAction("MembershipOptionConfirmation");
+            }
+
+            ViewBag.UserId = new SelectList(db.AspNetUsers, "Id", "Email", clubMembership.UserId);
+            ViewBag.ClubId = new SelectList(db.Clubs, "Id", "name", clubMembership.ClubId);
+            return View(clubMembership);
+        }
+
+        [Authorize]
+        // GET: ClubMemberships/MembershipOptionConfirmation/5
+        public ActionResult MembershipOptionConfirmation()
+        {
+            //DeSerialize
+            Club club = (Club)Session["Club Object"];
+            ClubMembership clubMembership = (ClubMembership)Session["ClubMembership Object"];
+            decimal price = (decimal)Session["Price"];
+
+            if (club == null || clubMembership == null || price == -10000)
+            {
+                return HttpNotFound();
+            }
+
+            ViewBag.ClubName = club.name;
+            ViewBag.ClubNickName = club.nickname;
+            ViewBag.Price = price;
+
+            string tempMessage = club.name + " " + clubMembership.MembershipOption + " " + "USD: " + price.ToString();
+            return Content(tempMessage);
+            //return View(clubMembership);
+        }
+
         // GET: ClubMemberships/Create
         public ActionResult Create()
         {
