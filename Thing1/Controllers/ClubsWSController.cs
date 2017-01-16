526851649e913e2aaeda645ffb41c21d79fc583f
suchintya <suchintya@gmail.com>
2017-01-16 03:33:12 -0800

    added webservices to clubs. disabled lazyloading to prevent circular loop. now all clubs can be listed and also particular club queried.

---
+++ Thing1/Controllers/ClubsWSController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Thing1.Models;
+
+namespace Thing1.Controllers
+{
+    public class ClubsWSController : ApiController
+    {
+        private user_managementEntities db = new user_managementEntities();
+
+        ClubsWSController()
+        {
+            db.Configuration.LazyLoadingEnabled = false;
+        }
+
+        // GET: api/ClubsWS
+        public IQueryable<Club> GetClubs()
+        {
+            return db.Clubs;
+        }
+
+        // GET: api/ClubsWS/5
+        [ResponseType(typeof(Club))]
+        public IHttpActionResult GetClub(int id)
+        {
+            Club club = db.Clubs.Find(id);
+            if (club == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(club);
+        }
+
+        // PUT: api/ClubsWS/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutClub(int id, Club club)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != club.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(club).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ClubExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/ClubsWS
+        [ResponseType(typeof(Club))]
+        public IHttpActionResult PostClub(Club club)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Clubs.Add(club);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (ClubExists(club.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = club.Id }, club);
+        }
+
+        // DELETE: api/ClubsWS/5
+        [ResponseType(typeof(Club))]
+        public IHttpActionResult DeleteClub(int id)
+        {
+            Club club = db.Clubs.Find(id);
+            if (club == null)
+            {
+                return NotFound();
+            }
+
+            db.Clubs.Remove(club);
+            db.SaveChanges();
+
+            return Ok(club);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ClubExists(int id)
+        {
+            return db.Clubs.Count(e => e.Id == id) > 0;
+        }
+    }
+}
