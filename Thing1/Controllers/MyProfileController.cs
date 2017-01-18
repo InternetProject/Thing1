6d4d20cfe3bd26a9c077094e5e10b931031c4856
Lucinda <Lucinda@DESKTOP-QN43E0E>
2017-01-18 11:08:07 -0800

    my profile 2

---
+++ Thing1/Controllers/MyProfileController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Thing1.Models;
+
+namespace Thing1.Controllers
+{
+    public class MyProfileController : Controller
+    {
+        private user_managementEntities db = new user_managementEntities();
+
+        // GET: MyProfile
+        public ActionResult Index()
+        {
+            if (!Request.IsAuthenticated)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.Unauthorized); // should change this later.
+            }
+            var userid = User.Identity.GetUserId();
+            AspNetUser myProfile = db.AspNetUsers.Find(userid);
+            if (myProfile == null)
+            {
+                return HttpNotFound();
+            }
+            return View(myProfile);
+        }
+
+        // GET: AspNetUsers/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AspNetUser aspNetUser = db.AspNetUsers.Find(id);
+            if (aspNetUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aspNetUser);
+        }
+
+        // POST: AspNetUsers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Email,EmailConfirmed,PasswordHash,SecurityStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEndDateUtc,LockoutEnabled,AccessFailedCount,UserName,LastName,FirstName,PreferredName,Program,TShirtSize")] AspNetUser aspNetUser)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(aspNetUser).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(aspNetUser);
+        }
+    }
+}
