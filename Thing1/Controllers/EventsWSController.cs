4e6b7e749a5f457a02756694c9491483dbf4427c
Ri <Ri@YURI>
2017-01-18 11:10:51 -0800

    eventWSController

---
+++ Thing1/Controllers/EventsWSController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Thing1.Models;
+
+namespace Thing1.Controllers
+{
+    public class EventsWSController : ApiController
+    {
+        private user_managementEntities db = new user_managementEntities();
+
+        // GET: api/EventsWS
+        public IQueryable<Event> GetEvents()
+        {
+            return db.Events;
+        }
+
+        // GET: api/EventsWS/5
+        [ResponseType(typeof(Event))]
+        public IHttpActionResult GetEvent(int id)
+        {
+            Event @event = db.Events.Find(id);
+            if (@event == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(@event);
+        }
+
+        // PUT: api/EventsWS/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutEvent(int id, Event @event)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != @event.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(@event).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EventExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/EventsWS
+        [ResponseType(typeof(Event))]
+        public IHttpActionResult PostEvent(Event @event)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Events.Add(@event);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = @event.ID }, @event);
+        }
+
+        // DELETE: api/EventsWS/5
+        [ResponseType(typeof(Event))]
+        public IHttpActionResult DeleteEvent(int id)
+        {
+            Event @event = db.Events.Find(id);
+            if (@event == null)
+            {
+                return NotFound();
+            }
+
+            db.Events.Remove(@event);
+            db.SaveChanges();
+
+            return Ok(@event);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool EventExists(int id)
+        {
+            return db.Events.Count(e => e.ID == id) > 0;
+        }
+    }
+}
