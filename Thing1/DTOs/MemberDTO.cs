ea305fd74e17dba4a47d627e915545e76472287a
Administrator <Administrator@EC2AMAZ-RKE4SPU>
2017-01-27 21:11:13 +0000

    New WS to get the list of officers of an specific club

---
+++ Thing1/DTOs/MemberDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Thing1.DTOs
+{
+    public class MemberDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Program { get; set; }
+    }
+}
