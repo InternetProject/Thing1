0a0cb7e3d5307c74b0f311775d76952a2613efe7
suchintya <suchintya@gmail.com>
2017-02-06 19:22:50 +0000

    added description for club officers

--- Thing1/DTOs/MemberDTO.cs
+++ Thing1/DTOs/MemberDTO.cs
@@ -11,5 +11,7 @@ namespace Thing1.DTOs
         public string LastName { get; set; }
         public string Email { get; set; }
         public string Program { get; set; }
+        public int RoleId { get; set; }
+        public string Description { get; set; }
     }
 }
