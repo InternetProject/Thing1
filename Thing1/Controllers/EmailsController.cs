8fce17aa0f5e683e08397916f7b76bea3f6d2d3f
siddharthshah1 <Siddharth.shah.2017@anderson.ucla.edu>
2017-02-13 10:19:31 -0800

    Added ability to email FEMBAs. Toggling the Everyone checkbox sets/clears other checkboxes. Setting/Clearing checkboxes generates a list of email addresses.

--- Thing1/Controllers/EmailsController.cs
+++ Thing1/Controllers/EmailsController.cs
@@ -44,32 +44,52 @@ namespace Thing1.Controllers
             return View();
         }
 
+        public JsonResult RecipientEmails(int? ClubId, String everyone, String directors, String vps, String ft1styears, String ft2ndyears, String fembas)
+        {
+            List<ClubMembership> recipients = RecipientList(ClubId, everyone, directors, vps, ft1styears, ft2ndyears, fembas);
+            if (recipients == null) return Json(new { Recipients= "ERROR" }, JsonRequestBehavior.DenyGet);
+
+            String recipientEmails = "";
+            foreach (ClubMembership recipient in recipients)
+            {
+                recipientEmails += ", " + recipient.AspNetUser.Email;
+            }
+            if (recipientEmails.Length == 0) recipientEmails = "   ";
+
+            return Json(new { Recipients=recipientEmails.Substring(2) }, JsonRequestBehavior.DenyGet);
+        }
+
+        private List<ClubMembership> RecipientList(int? ClubId, String everyone, String directors, String vps, String ft1styears, String ft2ndyears, String fembas)
+        {
+            string theUserId = User.Identity.GetUserId();
+            if (ClubId == null | ClubId == 0 | User.Identity == null | db.ClubMemberships.Where(c => c.AspNetUser.Id == theUserId & c.ClubId == ClubId & (c.Description.StartsWith("Director ") | c.Description.StartsWith("VP ") | c.Description.StartsWith("President"))).ToList().Count == 0)
+            {
+                return null;
+            }
+            return db.ClubMemberships.Where(c => (c.ClubId == ClubId) & (c.JoinDate < DateTime.Now & c.TermDate > DateTime.Now) & (everyone.Equals("on") | ((directors.Equals("on") & c.Description.StartsWith("Director ")) | (vps.Equals("on") & (c.Description.StartsWith("VP ") | c.Description.StartsWith("President"))) | (ft1styears.Equals("on") & c.AspNetUser.Program.Equals("FT " + DateTime.Now.Year + (DateTime.Now.Month < 7 ? 1 : 2))) | (ft2ndyears.Equals("on") & c.AspNetUser.Program.Trim().Equals("FT " + (DateTime.Now.Year + (DateTime.Now.Month < 7 ? 0 : 1)))))) | (fembas.Equals("on") & c.AspNetUser.Program.Trim().StartsWith("FEMBA"))).OrderBy(c => c.AspNetUser.Email).ToList();
+        }
+
         // POST: Emails/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create([Bind(Include = "Subject,Body")] int? ClubId, Email email, String everyone, String directors, String vps, String ft1styears, String ft2ndyears)
+        public async Task<ActionResult> Create([Bind(Include = "Subject,Body")] int? ClubId, Email email, String everyone, String directors, String vps, String ft1styears, String ft2ndyears, String fembas)
         {
             if (ModelState.IsValid)
             {
-                // terminate if user is not logged in or not an officer of the club
-                var theUserId = User.Identity.GetUserId();
-                if (ClubId == null | ClubId == 0 | User.Identity == null | db.ClubMemberships.Where(c => c.AspNetUser.Id == theUserId & c.ClubId == ClubId & (c.Description.StartsWith("Director ") | c.Description.StartsWith("VP ") | c.Description.StartsWith("President"))).ToList().Count == 0)
-                {
-                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-                }
+                List<ClubMembership> recipients = RecipientList(ClubId, everyone, directors, vps, ft1styears, ft2ndyears, fembas);
+                if (recipients == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
 
                 email.SentBy = User.Identity.GetUserId();
                 email.ClubId = (int)ClubId;
                 email.Sent = DateTime.Now;
 
-                var recipients = db.ClubMemberships.Where(c => (c.ClubId == ClubId) & (c.JoinDate < DateTime.Now & c.TermDate > DateTime.Now) & (everyone.Equals("on") | ((directors.Equals("on") & c.Description.StartsWith("Director ")) | (vps.Equals("on") & (c.Description.StartsWith("VP ") | c.Description.StartsWith("President"))) | (ft1styears.Equals("on") & c.AspNetUser.Program.Equals("FT " + DateTime.Now.Year + (DateTime.Now.Month < 7 ? 1 : 2))) | (ft2ndyears.Equals("on") & c.AspNetUser.Program.Trim().Equals("FT " + (DateTime.Now.Year + (DateTime.Now.Month < 7 ? 0 : 1))))))).ToList();
-                var message = new MailMessage();
+                MailMessage message = new MailMessage();
 
-                for (int i = 0; i < recipients.Count; i++)
+                foreach (ClubMembership recipient in recipients)
                 {
-                    message.Bcc.Add(new MailAddress(recipients[i].AspNetUser.Email));
+                    message.Bcc.Add(new MailAddress(recipient.AspNetUser.Email));
                 }
 
                 message.Subject = email.Subject;
@@ -84,16 +104,11 @@ namespace Thing1.Controllers
 
                 using (var smtp = new SmtpClient())
                 {
-                    var credential = new NetworkCredential
-                    {
-                        UserName = "the.internet.project.ucla@gmail.com",
-                        Password = "ivowelch"
-                    };
-                    smtp.Credentials = credential;
+                    smtp.Credentials = new NetworkCredential { UserName = "the.internet.project.ucla@gmail.com", Password = "ivowelch" };
                     smtp.Host = "smtp.gmail.com";
                     smtp.Port = 587;
                     smtp.EnableSsl = true;
-                    await smtp.SendMailAsync(message);
+                    smtp.Send(message);
                     return RedirectToAction("Index");
                 }
 
@@ -101,11 +116,6 @@ namespace Thing1.Controllers
             }
 
             return View(email);
-        }
-
-        private async Task<ActionResult> sendMessageAsync(MailMessage message)
-        {
-            return null;
         }
 
         // GET: Emails/Edit/5
